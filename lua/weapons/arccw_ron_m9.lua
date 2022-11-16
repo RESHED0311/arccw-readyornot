9599358da65bcb35bbae4b2043272a907dfb6dde
NotTrashraD <54300412+NotTrashraD@users.noreply.github.com>
2022-11-16 04:13:15 +0100

    M9

---
+++ lua/weapons/arccw_ron_m9.lua
@@ -0,0 +1,243 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - RON" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "M9"
+SWEP.TrueName = "Berreta M9FS"
+SWEP.Trivia_Class = "Assault Carbine"
+SWEP.Trivia_Desc = "the dreaded nword."
+SWEP.Trivia_Manufacturer = "Gryphon Arms"
+SWEP.Trivia_Calibre = "5.56x45mm NATO"
+SWEP.Trivia_Mechanism = "Gas-Operated"
+SWEP.Trivia_Country = "USA"
+SWEP.Trivia_Year = 1994
+
+SWEP.MirrorVMWM = true
+
+SWEP.WorldModelOffset = {
+    pos        =    Vector(-11, 4, -8),
+    ang        =    Angle(0, 0, 180),
+    bone    =    "ValveBiped.Bip01_R_Hand",
+	scale = 0.85
+}
+
+
+SWEP.Slot = 2
+
+if GetConVar("arccw_truenames"):GetBool() then
+    SWEP.PrintName = SWEP.TrueName
+    SWEP.Trivia_Manufacturer = "Armalite"
+end
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/m9ron.mdl"
+SWEP.WorldModel = "models/weapons/m9ron.mdl"
+SWEP.ViewModelFOV = 60
+
+SWEP.DefaultBodygroups = "000000"
+
+SWEP.Damage = 29
+SWEP.DamageMin = 20 -- damage done at maximum range
+SWEP.Range = 110 -- in METRES
+SWEP.Penetration = 7
+SWEP.DamageType = DMG_BULLET
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 900 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+SWEP.ChamberSize = 1 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 17 -- DefaultClip is automatically set.
+SWEP.ExtendedClipSize = 60
+SWEP.ReducedClipSize = 15
+
+SWEP.Recoil = 0.5
+SWEP.RecoilSide = 0.35
+SWEP.RecoilRise = 0.75
+
+SWEP.Delay = 60 / 750 -- 60 / RPM.
+SWEP.Num = 1 -- number of shots per trigger pull.
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+SWEP.NPCWeaponType = {"weapon_ar2", "weapon_smg1"}
+SWEP.NPCWeight = 150
+
+SWEP.AccuracyMOA = 5 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 500 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 150
+
+SWEP.Primary.Ammo = "smg1" -- what ammo type the gun uses
+SWEP.MagID = "stanag" -- the magazine pool this gun draws from
+
+SWEP.ShootVol = 110 -- volume of shoot sound
+SWEP.ShootPitch = 95 -- pitch of shoot sound
+
+SWEP.FirstShootSound = "weapons/pistols/m9/fire.wav"
+SWEP.ShootSound = "weapons/pistols/m9/fire.wav"
+SWEP.ShootSoundSilenced = "weapons/pistols/m9/firesil.wav"
+SWEP.DistantShootSound = "weapons/pistols/m9/firesil.wav"
+
+SWEP.MuzzleEffect = "muzzleflash_1"
+SWEP.ShellModel = "models/shells/shell_556.mdl"
+SWEP.ShellPitch = 95
+SWEP.ShellScale = 1.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+
+SWEP.SpeedMult = 0.96
+SWEP.SightedSpeedMult = 0.70
+SWEP.SightTime = 0.33
+
+SWEP.BulletBones = { -- the bone that represents bullets in gun/mag
+    -- [0] = "bulletchamber",
+    -- [1] = "bullet1"
+}
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-3.333, -2.941, 0),
+    Ang = Angle(0, 0, 0),
+    Magnification = 1.1,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "ar2"
+SWEP.HoldtypeSights = "rpg"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+
+SWEP.ActivePos = Vector(0, 0, 1)
+SWEP.ActiveAng = Angle(0, 0, -3)
+
+SWEP.HolsterPos = Vector(0.532, -6, 0)
+SWEP.HolsterAng = Angle(-7.036, 30.016, 0)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+
+SWEP.BarrelLength = 27
+
+
+
+SWEP.ExtraSightDist = 5
+
+SWEP.Attachments = {
+    {
+        PrintName = "Optic", -- print name
+        DefaultAttName = "Iron Sights",
+        Slot = "optic", -- what kind of attachments can fit here, can be string or table
+        Bone = "gun", -- relevant bone any attachments will be mostly referring to
+		VMScale = Vector(1.2, 1.2, 1.2),
+        Offset = {
+            vpos = Vector(0, 0, 0), -- offset that the attachment will be relative to the bone
+            vang = Angle(90, 0, -90),
+            wang = Angle(-9.738, 0, 180)
+        },
+        SlideAmount = { -- how far this attachment can slide in both directions.
+            -- overrides Offset.
+            vmin = Vector(0.05, -0.2, -4),
+            vmax = Vector(0.05, -0.2, 3),
+            wmin = Vector(5.36, 0.739, -5.401),
+            wmax = Vector(5.36, 0.739, -5.401),
+        },
+        -- CorrectivePos = Vector(-0.017, 0, -0.4),
+        CorrectivePos = Vector(0, 0, 0),
+        CorrectiveAng = Angle(0	, 0, 0)
+    },
+    {
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Slot = "muzzle",
+        Bone = "gun",
+        Offset = {
+            vpos = Vector(0, 2.1, 18),
+            vang = Angle(90, 0, -90),
+            wpos = Vector(26.648, 0.782, -8.042),
+            wang = Angle(-9.79, 0, 180)
+        },
+    },
+    {
+        PrintName = "Underbarrel",
+        Slot = {"foregrip"},
+        Bone = "gun",
+        Offset = {
+            vpos = Vector(0, 0, 0),
+            vang = Angle(90, 0, -90),
+            wpos = Vector(14.329, 0.602, -4.453),
+            wang = Angle(-10.216, 0, 180)
+        },
+        SlideAmount = {
+            vmin = Vector(0, 3.7, 7),
+            vmax = Vector(0, 3.7, 15),
+            wmin = Vector(15, 0.74, -4.301),
+            wmax = Vector(15, 0.74, -4.301),
+        }
+    },
+    {
+        PrintName = "Tactical",
+        Slot = "tac",
+        Bone = "gun",
+        Offset = {
+            vpos = Vector(-0.9, 2.4, 12), -- offset that the attachment will be relative to the bone
+            vang = Angle(90, 0, 180),
+            wpos = Vector(15.625, -0.253, -6.298),
+            wang = Angle(-8.829, -0.556, 90)
+        },
+    },
+}
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        Time = 1
+    },
+    ["fire"] = {
+        Source = "fire",
+		Time = 0.15
+    },
+    ["idle_empty"] = {
+        Source = "idle_empty",
+		Time = 0
+    },
+    ["reload"] = {
+        Source = "reload",
+        Time = 2.15,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        FrameRate = 60,
+		SoundTable = {
+            {s = "weapons/pistols/m9/magout.wav", t = 0.2},
+			{s = "weapons/pistols/m9/magin.wav", t = 1.7},
+        },
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 0.5,
+    },
+    ["reload_empty"] = {
+        Source = "reload_empty",
+        Time = 2.3,
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2,
+        Checkpoints = {24, 42, 59, 71, 89},
+        FrameRate = 60,
+		SoundTable = {
+            {s = "weapons/pistols/m9/magout.wav", t = 0.2},
+			{s = "weapons/pistols/m9/magin.wav", t = 1.7}, 
+			{s = "weapons/pistols/m9/slideforward.wav", t = 2},
+        },
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 0.5,
+    },
+}
